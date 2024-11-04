fffcb9ff7d536ca0cf860b6dc3261644b8698b16
Muharilmulyadi/alprog-e <arilmulyadi2@gmail.com>
2024-11-04 18:02:59 +0700

    Add files via upload

---
+++ tugas5.py
@@ -0,0 +1,10 @@
+print("nim = 242411164")
+print("nimAwal = 24")
+print("nimAkhir = 64")
+
+
+print("panjang = 64")
+print("lebar = 24")
+
+print(" hasil = 88 * panjang * lebar")
+print("hasil dari luas x {panjang} x {tinggi} adalah {hasil} =")
