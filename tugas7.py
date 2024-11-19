cba03b3349ffaa7575cd9eb8084f99b9494a3722
Muharilmulyadi/alprog-e <arilmulyadi2@gmail.com>
2024-11-19 17:16:36 +0700

    Add files via upload

---
+++ tugas7.py
@@ -0,0 +1,9 @@
+# menghitung luas persegi
+# nama : muh aril mulyadi
+
+print("menghitung luas persegi")
+panjang = int(input("masukan panjang : " ))
+lebar = int(input("masukan lebar : " ))
+
+hasil = panjang * lebar
+print (f"hasil dari {panjang} x {lebar} = {hasil}")
